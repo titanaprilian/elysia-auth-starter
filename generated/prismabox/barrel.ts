2f2ff3007bdc706507a5edf0099881fd978d8339
titanaprilian <titanaprilian73@gmail.com>
2026-02-19 20:07:53 +0700

    chore: remove dist and generated from git tracking

--- generated/prismabox/barrel.ts
+++
@@ -1,7 +0,0 @@
-export * from "./User";
-export * from "./RefreshToken";
-export * from "./Role";
-export * from "./Feature";
-export * from "./RoleFeature";
-export * from "./__nullable__";
-export * from "./__transformDate__";
